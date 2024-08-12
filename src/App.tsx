70c35837eeb048f55845df6714e01035fa88f78a
Mai <anh.nm210076@sis.hust.edu.vn>
2024-08-13 01:03:31 +0700

    feat: add antd theme config

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import theme from '@lib/antd/theme';
 import Home from '@pages/home';
 import Login from '@pages/login';
+import { ConfigProvider } from 'antd';
 import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <ConfigProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 };
 
